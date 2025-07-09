9e165e17576f78845177032f85ad29a4b177528b
Vyacheslav <va.povhleb@gmail.com>
2025-07-09 22:43:21 +0300

    refactor: linear prodject

--- linear_regression.py
+++ linear_regression.py
@@ -6,5 +6,17 @@ y = torch.tensor([2.0, 4.0, 6.0]) # y =  2x
 w = torch.tensor(0.0, requires_grad=True)
 b = torch.tensor(0.0, requires_grad=True)
 
+optimizer = torch.optim.SGD([w, b], lr=0.01)
 
+for epoch in range(100):
+    y_pred = w * x + b
 
+    loss = ((y_pred - y)**2).mean()
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if epoch % 10 == 0:
+        print(f'Epoch {epoch}, w={w.item():.3f}, b={b.item():.3f}, loss={loss.item():.3f}')
+
+print(f'Итог: w={w.item():.3f}, b={b.item():.3f}')
